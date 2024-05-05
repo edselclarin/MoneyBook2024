7f88173fe7d6dd14b0b165eec7c197fd8dad71f3
Edsel Clarin <edsel.clarin@yahoo.com>
2024-05-05 14:08:33 -0700

    Implement INotifyPropertyChanged

---
+++ MoneyBook.UI/ViewModel/MainViewModel.cs
@@ -0,0 +1,40 @@
+﻿using MoneyBook.Model;
+using MoneyBook.UI.Data;
+using System.Collections.ObjectModel;
+
+namespace MoneyBook.UI.ViewModel
+{
+    public class MainViewModel : ViewModelBase
+    {
+        private IInstitutionDataService institutionDataService_;
+
+        private Institution selectedInstitution_;
+        public Institution SelectedInstitution
+        {
+            get => selectedInstitution_;
+            set
+            {
+                selectedInstitution_ = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ObservableCollection<Institution> Institutions { get; set; }
+
+        public MainViewModel(IInstitutionDataService institutionDataService)
+        {
+            Institutions = new ObservableCollection<Institution>();
+            institutionDataService_ = institutionDataService;
+        }
+
+        public void Load()
+        {
+            Institutions.Clear();
+            var institutions = institutionDataService_.GetAll();
+            foreach (var institution in institutions)
+            {
+                Institutions.Add(institution);
+            }
+        }  
+    }
+}
